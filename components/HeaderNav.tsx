541c5448cf2b4972e6f11b3bfa2acbd9d9ca8a02
Zakk <zakk@hawthorne.io>
2023-06-05 16:00:03 -0500

    Get a basic headernav up on the screen

---
+++ components/HeaderNav.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function HeaderNav() {
+
+  return (
+    <div className="flex flex-row justify-between items-center p-4">
+      <div>
+        <h1>Sidevents.lol</h1>
+      </div>
+      <div>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  )
+}
